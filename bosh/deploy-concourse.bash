08146f13d5fd44e8a15f4de60223c6a33425e1d3
Eitan Suez <esuez@pivotal.io>
2019-06-10 12:46:58 -0500

    sample bosh deploy command for concourse (cluster)

---
+++ bosh/deploy-concourse.bash
@@ -0,0 +1,12 @@
+cd concourse-bosh-deployment/cluster
+bosh deploy --deployment concourse concourse.yml \
+  --vars-file ../versions.yml \
+  --vars-file concourse-vars-file.yml \
+  --ops-file operations/basic-auth.yml \
+  --ops-file operations/privileged-http.yml \
+  --ops-file operations/privileged-https.yml \
+  --ops-file operations/tls.yml \
+  --ops-file operations/tls-vars.yml \
+  --ops-file operations/web-network-extension.yml \
+  --ops-file operations/scale.yml \
+  --ops-file operations/worker-ephemeral-disk.yml
