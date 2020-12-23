f95c4a446e034a2f6f2d30e422a8399dace83298
Eitan Suez <esuez@pivotal.io>
2020-12-23 11:25:27 -0600

    pal4pe vsphere - updating product versions, removing controlplane tile from download script

--- vsphere/paving-control-plane/download_controlplane_all.sh
+++ vsphere/paving-control-plane/download_controlplane_all.sh
@@ -4,12 +4,10 @@ if [ -z "$PIVNET_TOKEN" ]; then
     echo "Must provide pivnet api token as environment variable PIVNET_TOKEN"
     exit 1
 fi
-ops_manager_version=2.9.5
-controlplane_version=1.0.3-beta.1
-controlplane_stemcell_version=315.70
+ops_manager_version=2.10.4
 platform_automation_version=4.4.3
-minio_version=1.0.6
-minio_stemcell_version=315.41
+minio_version=1.0.12
+minio_stemcell_version=621.94
 files_directory=~/workspace/products
 
 mkdir -p ${files_directory}
@@ -21,27 +19,6 @@ if [ ! -s ${files_directory}/ops-manager-vsphere-${ops_manager_version}*.ova ]; 
     pivnet download-product-files -p ops-manager -r ${ops_manager_version} -g "*.ova" -d ${files_directory}
 else
   echo "Ops Manager version $ops_manager_version already exists"
-fi
-
-echo "Attempting to download Platform Automation Engine Tile version $controlplane_version"
-if [ ! -s ${files_directory}/control-plane-${controlplane_version}*.pivotal ]; then
-    pivnet download-product-files -p p-control-plane-components -r ${controlplane_version} -g "*.pivotal" -d ${files_directory}
-else
-  echo "Platform Automation Engine Tile version $controlplane_version already exists"
-fi
-
-echo "Attempting to download Stemcell version $controlplane_stemcell_version"
-if [ ! -s ${files_directory}/bosh-stemcell-${controlplane_stemcell_version}-*.tgz ]; then
-    pivnet download-product-files -p stemcells-ubuntu-xenial -r ${controlplane_stemcell_version} -g *vsphere* -d ${files_directory}
-else
-  echo "Stemcell version $controlplane_stemcell_version already exists"
-fi
-
-echo "Attempting to download Stemcell version $minio_stemcell_version"
-if [ ! -s ${files_directory}/bosh-stemcell-${minio_stemcell_version}-*.tgz ]; then
-    pivnet download-product-files -p stemcells-ubuntu-xenial -r ${minio_stemcell_version} -g *vsphere* -d ${files_directory}
-else
-  echo "Stemcell version $minio_stemcell_version already exists"
 fi
 
 echo "Attempting to download Platform Automation Image version $platform_automation_version"
@@ -56,5 +33,12 @@ if [ ! -s ${files_directory}/minio-internal-blobstore-${minio_version}*.pivotal 
     pivnet download-product-files -p minio-internal-blobstore -r ${minio_version} -g "*.pivotal" -d ${files_directory}
 else
   echo "Minio tile version $minio_version already exists"
+fi
+
+echo "Attempting to download Stemcell version $minio_stemcell_version"
+if [ ! -s ${files_directory}/bosh-stemcell-${minio_stemcell_version}-*.tgz ]; then
+    pivnet download-product-files -p stemcells-ubuntu-xenial -r ${minio_stemcell_version} -g *vsphere* -d ${files_directory}
+else
+  echo "Stemcell version $minio_stemcell_version already exists"
 fi
 
